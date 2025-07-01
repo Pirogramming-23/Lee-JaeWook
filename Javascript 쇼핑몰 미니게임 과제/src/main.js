c233adc8c20a76d8198229aa6e14f6c0d97bd5cd
ljo1010 <ljo111004@gmail.com>
2025-07-02 00:47:26 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,19 +1,31 @@
-
 // fetch the items
 function loadItems(){
     return fetch('data/data.json')
     .then(response => response.json(    ))
     .then(json => json.items);
-
 }
 
+//update the list with the given items 
+function displayItems(items){
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item)).join('');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
 
-
+//create html list item from the given data item
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+    `;
+}
 //main
 loadItems()
     .then(items => {
         console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListeners(items)
     })
     .catch(console.log)

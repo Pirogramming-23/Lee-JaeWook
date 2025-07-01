7dd13131bbfb8dad3f5095c936fa6197457369d8
ljo1010 <ljo111004@gmail.com>
2025-07-02 01:12:58 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -21,11 +21,31 @@ function createHTMLString(item) {
         </li>
     `;
 }
+
+function onButtonClick(event, items){
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null){
+        return;
+    }
+    
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+function setEventListeners(items){
+    const logo = document.querySelector('.logo');
+    const button = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    button.addEventListener('click', event => onButtonClick(event, items));
+}
 //main
 loadItems()
     .then(items => {
         console.log(items);
         displayItems(items);
-        // setEventListeners(items)
+        setEventListeners(items)
     })
     .catch(console.log)

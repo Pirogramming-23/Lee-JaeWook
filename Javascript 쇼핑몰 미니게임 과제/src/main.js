42b68ce9bd3297a427e3b8bed953eb71e069dbea
ljo1010 <ljo111004@gmail.com>
2025-07-02 00:28:28 +0900

    JavaScript - fetch

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,1 +1,19 @@
-[]
+
+// fetch the items
+function loadItems(){
+    return fetch('data/data.json')
+    .then(response => response.json(    ))
+    .then(json => json.items);
+
+}
+
+
+
+//main
+loadItems()
+    .then(items => {
+        console.log(items);
+        // displayItems(items);
+        // setEventListeners(items)
+    })
+    .catch(console.log)

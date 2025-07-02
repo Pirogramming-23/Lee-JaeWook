fa8696ebbd848abf876587109929cae969549cd6
ljo1010 <ljo111004@gmail.com>
2025-07-02 17:45:40 +0900

    게임 초기화 기능

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -1,7 +1,6 @@
 // @ts-nocheck
 // TODO:버튼 누르고 시도 9번 다 써버리면 게임 오버
 // 정답은 임의로 설정(나중에 난수로 만들 예정)
-const answer = ['4', '7', '2']
 let attempts = 9;
 document.getElementById("attempts").innerText = attempts;
 const text = `남은 횟수: ${attempts}`;
@@ -12,13 +11,20 @@ const text = `남은 횟수: ${attempts}`;
 // 기회 9번 정답 맞추면 승리 기회 떨어지면 패배
 // html 아이디 가져와서 변수 만들기
 function check_numbers(){
+
     const value1 = document.getElementById("number1").value;
     const value2 = document.getElementById("number2").value;
     const value3 = document.getElementById("number3").value;
 
+    
     const input = [value1, value2, value3]
     console.log(answer)
     console.log(value1, value2, value3)    
+    
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+    document.getElementById("number3").value = "";
+
     let strike = 0;
     let ball = 0;
 
@@ -40,19 +46,19 @@ function check_numbers(){
     // result id를 받아서 결과출력
     let resultText = `입력한 값:${input.join("")} = `;
     if(strike === 0 && ball === 0){
-        resultText += "O";
+        resultText += `<span class = "out">O</span>`;
     }
     else{
         if (strike > 0){
-            resultText += `${strike}S`;
+            resultText += `${strike}<span class = "strike">S</span>`;
         }
         if(ball > 0)
         {
-            resultText += `${ball}B`;
+            resultText += `${ball}<span class = "ball">B</span>`;
         }
     }
     const resultDiv = document.getElementById("results");
-    resultDiv.innerText += resultText + `\n`;
+    resultDiv.innerHTML += resultText + `<br>`;
 
     // TODO:attemps를 다 쓰게 될 시에 게임오버png출력 또는 스또라이크 3개일 시 승리 
     if(attempts === 0){
@@ -75,3 +81,20 @@ function check_numbers(){
         document.querySelector(".submit-button").style.backgroundColor = "gray";
     }
 }
+
+// TODO:중복이 생기지 않는 난수 3개
+// 포인트는 rand는 숫자, strRand는 문자열이라는 거
+// includes는 값자체를 비교함
+function generateAnswer(){
+    const nums = [];
+    while(nums.length < 3){
+        const rand = Math.floor(Math.random() * 9) + 1;
+        const strRand = rand.toString();
+        if (!nums.includes(strRand)){
+            nums.push(strRand);
+        }
+    }
+    return nums;
+}
+// 함수 생성 뒤에 선언
+const answer = generateAnswer();

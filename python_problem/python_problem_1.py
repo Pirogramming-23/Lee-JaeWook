3565900550d36c538c75c2d16cfa96fe2ddf81f8
ljo1010 <ljo111004@gmail.com>
2025-07-04 02:10:54 +0900

    4단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -16,3 +16,12 @@ while(True):
         print("1, 2, 3 중 하나를 입력하세요")
     else:
         break
+# 4. 변수 num 을 이용하여, 2단계에서 입력한 수만큼 숫자를 출력하는 코드를 작성하여라.
+# 예를 들어, 2단계에서 playerA가 3을 입력했다면, 1,2,3을 아래와 같은 형식으로 출력하여라.
+# playerA : 1
+# playerA : 2
+# playerA : 3
+
+for _ in range(ans):
+    num += 1
+    print(f"playerA : {num}")
